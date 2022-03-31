d1d08dc787e1099009fb5b5847b84f1f5d800a27
Cristaudo Oscar <o.cristaudo.1296@vallauri.edu>
2022-03-31 21:40:17 +0200

    Gestione Accesso 99%
    Gestita la registrazione e il login tramite pin il pin va reinserito ogni volta che si chiude l'app

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,11 @@
 $(()=>{
     //CONTROLLO SE SONO NEL PRIMO AVVIO DELL'APP
-    let User=localStorage.getItem('Utente');
+    let User=JSON.parse(localStorage.getItem('Utente'));
     if(User==null)
         window.location.href="../page/first.html";
-    else
-        alert('Registrazione effettuata con successo')
+    else{
+        //controllo reinserimento pc a chiusura app
+        if(sessionStorage.getItem('Available')==null)
+            window.location.href="../page/login.html";
+    }
 })
--- www/js/register.js
+++ www/js/register.js
@@ -1,12 +1,46 @@
+let dati=[];
 $(()=>{
-    //Inserimento Guidato dati utente
+    //Inserimento Guidato dati utente PRIMO AVVIO
     $('#btnRegister').click(()=>{
-        let User={
-            Name:$('#txtNome').val(),
-            Cognome:$('#txtCognome').val(),
-            DataN:$('#txtEta').val()
-        };
-        localStorage.setItem('Utente',User);
-        window.location.href='../index.html';
+        if($('#btnRegister').val()!='Registrati'){
+            if($('#txtNome').val().trim()!=""){
+                dati.push($('#txtNome').val());
+                $('#info').hide(1000,function(){
+                    $('#info').html('').append('<label for="txtPin" class="form-label">Inserisci un codice di 4 cifre</label><input type="number" class="form-control" id="txtPin" placeholder="Inserisci il PIN" required>').show(1000)
+                    $('#btnRegister').val('Registrati')
+                });
+            }
+            else{
+                $('#txtNome').attr("placeholder", "Campo Obbligatorio").addClass('error').focus();
+            }
+        }
+        else{
+            if($('#txtPin').val().trim()!=""){
+                dati.push($('#txtPin').val());
+                let User={Nome:dati[0],PIN:dati[1]};
+                localStorage.setItem('Utente',JSON.stringify(User));
+                window.location.href='login.html';
+            }
+            else
+                $('#txtPin').attr("placeholder", "Campo Obbligatorio").addClass('error').focus();
+        }
+    })
+
+    //Controllo PIN Utente
+    let Nome=JSON.parse(localStorage.getItem('Utente')).Nome;
+    if(Nome!=null)
+        $('#subTit').text('Ciao '+Nome);
+
+    $('#btnVerify').click(()=>{
+        let PIN=JSON.parse(localStorage.getItem('Utente')).PIN;
+        if($('#txtPin').val().trim()!="")
+            if($('#txtPin').val().trim()==PIN){
+                sessionStorage.setItem('Available',true);
+                window.location.href='../index.html';
+            }
+            else
+                $('#txtPin').attr("placeholder", "PIN errato").addClass('error').focus().val('');
+        else
+            $('#txtPin').attr("placeholder", "Campo Obbligatorio").addClass('error').focus();
     })
 })
